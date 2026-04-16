12aad70dd20b9e5eca27166c65638e961e7826f2
Arimerli <arianna.merli@fermi.mo.it>
2026-04-16 19:07:56 +0200

    inizio configurazione chiaro-scuro con variabili per colori css

--- magazine/componenti/models.py
+++ magazine/componenti/models.py
@@ -1,89 +1,1 @@
-from django.db import models
-
-
-class Categories(models.Model):
-    nome = models.CharField(max_length=255)
-    parent = models.ForeignKey('self', models.DO_NOTHING, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'categories'
-
-
-class Components(models.Model):
-    nome = models.CharField(max_length=255)
-    categoria = models.ForeignKey(Categories, models.DO_NOTHING, db_column='categoria')
-    pezzi = models.IntegerField()
-    link = models.CharField(max_length=255)
-
-    class Meta:
-        managed = False
-        db_table = 'components'
-
-
-class Esperienze(models.Model):
-    nome = models.CharField(max_length=255)
-    descrizione = models.CharField(max_length=255, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'esperienze'
-
-
-class EsperienzeComponents(models.Model):
-    esperienza = models.ForeignKey(Esperienze, models.DO_NOTHING)
-    component = models.ForeignKey(Components, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'esperienze_components'
-
-
-class Giacenze(models.Model):
-    componente = models.ForeignKey(Components, models.DO_NOTHING)
-    cassetto = models.ForeignKey('Locations', models.DO_NOTHING)
-    quantita = models.IntegerField()
-    min_quantita = models.IntegerField()
-    scorta = models.BooleanField()
-
-    class Meta:
-        managed = False
-        db_table = 'giacenze'
-
-
-class Locations(models.Model):
-    nome = models.CharField(max_length=255)
-    parent = models.ForeignKey('self', models.DO_NOTHING, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'locations'
-
-
-class TagComponents(models.Model):
-    tag = models.ForeignKey('Tags', models.DO_NOTHING)
-    component = models.ForeignKey(Components, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'tag_components'
-
-
-class Tags(models.Model):
-    caratteristica = models.CharField(max_length=255)
-
-    class Meta:
-        managed = False
-        db_table = 'tags'
-
-
-class Users(models.Model):
-    nome = models.CharField(max_length=255)
-    cognome = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
-    password = models.CharField(max_length=255)
-    permessi = models.IntegerField()
-
-    class Meta:
-        managed = False
-        db_table = 'users'
+from django.db import modelsclass Categories(models.Model):    nome = models.CharField(max_length=255)    parent = models.ForeignKey('self', models.DO_NOTHING, blank=True, null=True)    class Meta:        managed = False        db_table = 'categories'class Components(models.Model):    nome = models.CharField(max_length=255)    categoria = models.ForeignKey(Categories, models.DO_NOTHING, db_column='categoria')    pezzi = models.IntegerField()    link = models.CharField(max_length=255)    class Meta:        managed = False        db_table = 'components'class Esperienze(models.Model):    nome = models.CharField(max_length=255)    descrizione = models.CharField(max_length=255, blank=True, null=True)    class Meta:        managed = False        db_table = 'esperienze'class EsperienzeComponents(models.Model):    esperienza = models.ForeignKey(Esperienze, models.DO_NOTHING)    component = models.ForeignKey(Components, models.DO_NOTHING)    class Meta:        managed = False        db_table = 'esperienze_components'class Giacenze(models.Model):    componente = models.ForeignKey(Components, models.DO_NOTHING)    cassetto = models.ForeignKey('Locations', models.DO_NOTHING)    quantita = models.IntegerField()    min_quantita = models.IntegerField()    scorta = models.BooleanField()    class Meta:        managed = False        db_table = 'giacenze'class Locations(models.Model):    nome = models.CharField(max_length=255)    parent = models.ForeignKey('self', models.DO_NOTHING, blank=True, null=True)    class Meta:        managed = False        db_table = 'locations'class TagComponents(models.Model):    tag = models.ForeignKey('Tags', models.DO_NOTHING)    component = models.ForeignKey(Components, models.DO_NOTHING)    class Meta:        managed = False        db_table = 'tag_components'class Tags(models.Model):    caratteristica = models.CharField(max_length=255)    class Meta:        managed = False        db_table = 'tags'class Users(models.Model):    nome = models.CharField(max_length=255)    cognome = models.CharField(max_length=255)    email = models.CharField(max_length=255)    password = models.CharField(max_length=255)    permessi = models.IntegerField()    class Meta:        managed = False        db_table = 'users'
